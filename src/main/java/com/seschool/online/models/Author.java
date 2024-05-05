7caa1cc019a4137e43ec979f73839b9bace828c7
ayoung <saing>
2024-05-05 17:09:08 +0700

    add Author

---
+++ src/main/java/com/seschool/online/models/Author.java
@@ -0,0 +1,48 @@
+package com.seschool.online.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(
+            name = "f_name"
+    )
+    private String firstName;
+
+    @Column(
+            name = "l_name"
+    )
+    private String lastName;
+
+    @Column(
+            unique = true,
+            nullable = false,
+            length = 30
+    )
+    private String email;
+    private Integer age;
+
+    @Column(
+            updatable = false,
+            nullable = false
+    )
+    private LocalDateTime createdAt;
+
+    @Column(
+            insertable = false
+    )
+    private LocalDateTime lastModied;
+
+}
